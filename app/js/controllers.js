13979fb7cfcb7d245c04e3454a56c87db4f061dc
Nicola Cimmino <nicola@nicolacimmino.com>
2014-05-25 12:40:17 +0300

    Cleanup

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,14 +1,33 @@
+/* controllers.js is part of ExpensesWebClient SPA contains the controllers.
+ *
+ *   Copyright (C) 2014 Nicola Cimmino
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ */
 'use strict';
 
-/* Controllers */
-
-  
+// Define the expensesController which depends on expensesAPIservice.
+// This controller is responsible to provide expenses data to the scope.
 angular.module('ExpensesWebClient.controllers', []).
   controller('expensesController', function($scope, expensesAPIservice) {
-    $scope.nameFilter = null;
+    
     $scope.expensesList = [];
-
+	
+	// Asyncronously fetch expenses from the API and report them to the scope.
     expensesAPIservice.getExpenses().success(function (response,status, headers, config) {
         $scope.expensesList = response;
     });
+	
   });
--- app/js/services.js
+++ app/js/services.js
@@ -1,13 +1,34 @@
+/* controllers.js is part of ExpensesWebClient SPA contains the controllers.
+ *
+ *   Copyright (C) 2014 Nicola Cimmino
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ */
 'use strict';
 
-/* Services */
-
-
+// Add a factory to encapsulate the Expenses API.
+// Note that the API has CORS enabled so we don't need to use
+//	JSOND on the client side to circumvent the same origin policy. 
+//
 angular.module('ExpensesWebClient.services', []).
   factory('expensesAPIservice', function($http) {
 
     var expensesAPIservice = {};
 
+	// Get all expenses.
+	// Currently there is no pagination in the API, this will change.
     expensesAPIservice.getExpenses = function() {
       return $http({
         method: 'GET', 
